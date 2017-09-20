66a7555cd0df3ff4799d92af4825bb69fbe6061e
dupontbertrand <dupontbertrand59@gmail.com>
2017-09-20 10:11:41 +0200

    initital commit

---
+++ components/Feednews.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-md-4">
+      <h3>Lalallalalalalalalal</h3>
+    </div>
+
+</template>
+
+<script>
+export default{
+  
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,96 @@
+<template>
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="#">Proto visu</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Don't know</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/javascript">#Javascript <span class="badge badge-primary">Hashtag</span></a>
+          <a class="dropdown-item" href="#">Quincy Larson <span class="badge badge-info">Personne</span></a>
+          <a class="dropdown-item" href="#">Lille <span class="badge badge-success">Lieu</span></a>
+          <a class="dropdown-item" href="#">Dunno <span class="badge badge-warning">Warning</span></a>
+          <a class="dropdown-item" href="#">Je sais paaaas <span class="badge badge-dark">Dark</span></a>
+        </div>
+      </li>
+      <button class="btn">
+        Quoi de neuf ? <span class="badge badge-secondary">9</span>
+        <span class="sr-only">unread messages</span>
+      </button>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  <div class="container">
+    <nuxt/>
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
