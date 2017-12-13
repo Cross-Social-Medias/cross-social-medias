1e512b8916ea3798661049df237c46f0b35f554d
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-12-13 10:03:42 +0100

    update view by adding news data

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,43 @@
 <template>
   <section class="container">
     <div class="row">
-      <feednews/>
-      <feednews/>
-      <feednews/>
+      <feednews
+        v-for="provider in providers"
+        :key="provider.id"
+        :provider="provider"
+      />
     </div>
   </section>
+  <footer/>
+    <nuxt-link to="/about">About page</nuxt-link>
+  </footer>
 </template>
 
 <script>
   import Feednews from '~/components/Feednews.vue'
+  let nextTodoId = 1
 
   export default {
     components: {
       Feednews
+    },
+    data () {
+      return {
+        providers: [
+          {
+            id: nextTodoId++,
+            text: 'Twitter'
+          },
+          {
+            id: nextTodoId++,
+            text: 'Instagram'
+          },
+          {
+            id: nextTodoId++,
+            text: 'Facebook'
+          }
+        ]
+      }
     }
   }
 </script>
