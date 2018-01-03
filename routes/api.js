9e979270785df81f9f51c823ccdc9ceb7e466b0e
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-04 00:17:26 +0100

    fix middleware

--- routes/api.js
+++ routes/api.js
@@ -4,17 +4,17 @@ require('dotenv').config();
 
 const app = express();
 const client = new Twitter({
-  consumer_key: process.env,
-  consumer_secret: process.env,
-  access_token_key: process.env,
-  access_token_secret: process.env
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret,
 });
 
 app.get('/', (req, res) => {
-  // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+  // // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
   client.get('statuses/user_timeline', { screen_name: 'nodejs', count: 20 }, (error, tweets, response) => {
     if (!error) {
-      res.status(200).render('index', { title: 'Express', tweets: tweets });
+      res.status(200).json({ title: 'Express', tweets: tweets });
     }
     else {
       res.status(500).json({ error: error });
