010bbd7094d2014ed0d7ef3c2fb329b681f7439b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-08 00:26:09 +0200

    create new page to render tweets

---
+++ pages/twitter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+      <ul id="example-1">
+  	  <li v-for="tweet in tweets">
+        {{ tweet.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  async asyncData ({ params }) {
+    const username = "AnthonyLastella";
+    console.log(process.env.baseUrl);
+    let { data } = await axios.get(`${process.env.baseUrl}/twitter_entry_point?username=${username}`)
+    return { tweets: data.tweets, title: data.title }
+  }
+}
+</script>
