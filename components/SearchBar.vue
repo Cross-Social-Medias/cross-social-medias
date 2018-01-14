b0402418e327899bf18439cecdd7be4cdc204a49
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-15 00:16:39 +0100

    create searchBar component

---
+++ components/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    search: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
