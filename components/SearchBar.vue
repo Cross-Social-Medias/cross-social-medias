a9c275bcf4e2d64f0acf3b04df1cc66855966a77
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-15 00:21:20 +0100

    add m-model attribute

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+    <input v-model="search"class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </span>
 </template>
