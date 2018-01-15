8796809d96754e47b20d89a9d021ddd73316c2ea
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-16 00:56:05 +0100

    add getter in researhc reducer

--- store/research.js
+++ store/research.js
@@ -2,8 +2,13 @@ export const state = () => ({
   research: ""
 })
 
+export const getters = {
+  getResearch: state => state.research
+}
+
 export const mutations = {
   update (state, text) {
+    console.log(text);
     state.research = text;
   }
 }
