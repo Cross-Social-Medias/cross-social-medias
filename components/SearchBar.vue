107bf13c9413c62b915abf2713386212fd98d537
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-15 23:49:25 +0100

    remove props and notify parent that a new reseach was done

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,16 +1,21 @@
 <template>
   <span>
-    <input v-model="search"class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" v-model="search">
+    <button class="btn btn-outline-success my-2 my-sm-0" @click="submitResearch">Search</button>
   </span>
 </template>
 
 <script>
 export default {
-  props: {
-    search: {
-      type: String,
-      required: true
+  data () {
+    return {
+      search: "AnthonyLastella"
+    }
+  },
+  methods: {
+    submitResearch (e) {
+      this.$emit("on-submit", this.search);
+      e.preventDefault();
     }
   }
 }
