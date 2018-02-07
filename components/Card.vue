aa3da39bec64cb326aa781e51f14f3bff24fe0c3
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-07 23:56:45 +0100

    divide card and card content

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,18 @@
 <template>
     <div class="card">
-      <div v-if="infos.entities.media">
-        <div v-for="(val, key, index) in infos.entities.media">
-          <img :src="infos.entities.media[0].media_url_https" style="height:70px;width:auto"/>
-        </div>
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">{{ infos.text }}</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">{{ infos.created_at }}</small>
+      <div v-if="kind === 'twitter'">
+        <twitter-content-card :twitter="infos" />
       </div>
     </div>
 </template>
 
 <script>
-
+import TwitterContentCard from '~/components/TwitterContentCard.vue';
 export default {
+
+  components: {
+    TwitterContentCard
+  },
   props: {
     infos: {
       type: Object,
---
+++ components/TwitterContentCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+      <div v-if="twitter.entities.media">
+        <div v-for="(val, key, index) in twitter.entities.media">
+          <img :src="twitter.entities.media[0].media_url_https" style="height:70px;width:auto"/>
+        </div>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">{{ twitter.text }}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">{{ twitter.created_at }}</small>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    twitter: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
