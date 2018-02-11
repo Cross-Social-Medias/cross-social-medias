02a8cbae817d49741bd287ee7f7bef21c6d6d187
dupontbertrand <dupontbertrand59@gmail.com>
2018-02-11 21:40:13 +0100

    change style and add video support

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,11 @@
+<style media="screen">
+    .card {
+        box-shadow:0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12)
+    }
+</style>
+
 <template>
-    <div class="card text-white bg-dark mb-3">
+    <div class="card mb-3">
       <div v-if="kind === 'twitter'">
         <twitter-content-card :twitter="infos" />
       </div>
--- components/TwitterContentCard.vue
+++ components/TwitterContentCard.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <div class="card-body">
-      <div v-if="twitter.entities.media">
-        <div v-for="(val, key, index) in twitter.entities.media">
-          <img class="card-img-top" :src="val.media_url_https" alt="Card image cap">
+      <div v-if="twitter.extended_entities">
+        <div v-for="(val, key, index) in twitter.extended_entities.media">
+            <div v-if="val.type == 'video' ">
+                <div v-if="val.video_info.variants">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <video class="embed-responsive-item" controls :poster="val.media_url_https">
+                            <source :src="val.video_info.variants[0].url" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+            <div v-else-if="val.type == 'photo' ">
+                <img class="card-img-top" :src="val.media_url_https" alt="Card image cap">
+            </div>
         </div>
       </div>
       <p class="card-text">{{ twitter.text }}</p>
