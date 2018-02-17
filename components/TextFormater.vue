6b7132f519d6fee37b0e934ec76294ed4f60f2dc
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-17 13:40:25 +0100

    parse htag, username and url

--- components/TextFormater.vue
+++ components/TextFormater.vue
@@ -13,9 +13,26 @@
     },
     computed: {
       convertedText() {
+        return this.parseHashtag(this.parseUsername(this.parseUrl(this.text)));
+      }
+    },
+    methods: {
+      parseUrl(text) {
         const urlRegex = /(https?:\/\/[^\s]+)/g;
-        return this.text.replace(urlRegex, function(url) {
+        return text.replace(urlRegex, function(url) {
           return '<a href="' + url + '">' + url + '</a>';
+        });
+      },
+      parseUsername(text) {
+        return text.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+          var username = u.replace("@", "");
+          return u.link("https://twitter.com/" + username);
+        });
+      },
+      parseHashtag(text) {
+        return text.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+          var tag = t.replace("#", "");
+          return t.link("https://twitter.com/hashtag/" + tag);
         });
       }
     }
