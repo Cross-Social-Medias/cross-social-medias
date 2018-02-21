132b4f00d7377d5b15328ccfff9563c0185a5a3f
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-22 00:48:27 +0100

    add new store action to mock mappings

--- store/mappings.js
+++ store/mappings.js
@@ -16,13 +16,21 @@ export const mutations = {
 
 export const actions = {
   fetchMappings({ commit }) {
-    axios.get(`http://localhost:4000/api/v1/social_media_mappings`)
+    axios.get(`link_api/api/v1/social_media_mappings`)
       .then(response => {
-        const { mappings } = response.data;
-        commit('FETCH_MAPPINGS', mappings);
+        const { data } = response.data;
+        commit('FETCH_MAPPINGS', data);
       })
       .catch(e => {
         commit('ADD_ERROR', "Something went wrong. The mappings does not exist or there is a network issue. Please Try again !");
       });
+  },
+  fetchMappingsMock({ commit }) {
+    const mock = [
+      { "twitter_username": "AnthonyLastella", "mapping_name": "Anthony Lastella", "instagram_username": "anthonyLastella" },
+      { "twitter_username": "Ber", "mapping_name": "Bertrand Dupond", "instagram_username": "fake_insta" },
+      { "twitter_username": "zemog_emualluig", "mapping_name": "Guillaume Gomez", "instagram_username": "fake_insta2" }
+    ];
+    commit('FETCH_MAPPINGS', mock);
   }
 }
