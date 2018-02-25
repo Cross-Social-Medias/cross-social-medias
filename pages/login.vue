ef39aac362d2b398c53e73461587a28e4c3f1c8b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-25 16:19:42 +0100

    update alert creation + fix redirection

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Log In</h1>
-    <form v-on:submit="submit_login"  method="post">
-      <div v-if="alert" class="alert alert-danger" role="alert">
+    <form @submit.prevent="submit_login"  method="post">
+      <div v-if="alert" :class="['alert', 'alert-' + alert.type]" role="alert">
         {{alert.message}}
       </div>
       <div class="form-group">
@@ -30,22 +30,21 @@
       }
     },
     methods: {
-      submit_login (e) {
-        e.preventDefault();
+      submit_login () {
         this.alert = null;
         this.loading = true;
         // this.$store.dispatch('auth/login', {
         this.$store.dispatch('auth/loginMock', {
           email: this.email,
           password: this.password
-        }).then(result => {
+        }).then((result) => {
           this.alert = { type: 'success', message: result.data.message };
           this.loading = false;
           this.$router.push({ path: 'admin' });
         }).catch(error => {
           this.loading = false;
           if (error.response && error.response.data) {
-            this.alert = {type: 'error', message: error.response.data.message || error.reponse.status};
+            this.alert = {type: 'danger', message: error.response.data.message || error.reponse.status};
           }
         })
       }
