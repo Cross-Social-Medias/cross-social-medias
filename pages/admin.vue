e080a982d37b2e1181b3f7499ac5c2ec39d9ec88
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-25 17:46:38 +0100

    move check me button

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
     <h1>Admin page</h1>
-    <p v-if="user">Hello, {{user.email}}</p>
-    <p v-else>The user is not authenticated!</p>
+    <div v-if="user">
+      <p>Hello, {{user.email}}</p>
+      <button class="btn btn-primary" role="button" @click="logOut">Log out</button>
+    </div>
+    <div v-else>
+      <p>The user is not authenticated!</p>
+    </div>
+    <br/>
+    <button class="btn btn-primary" role="button" @click="checkMe">Check Me</button>
   </div>
 </template>
 <script>
 export default {
   computed: {
     user () { return (this.$store.state.auth || {}).user || null }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/reset')
+        .then(() => {
+          this.$router.push('/');
+        });
+    },
+    checkMe() {
+      // this.$store.dispatch('auth/fetch');
+      this.$store.dispatch('auth/fetchMock');
+    }
   }
 }
 </script>
