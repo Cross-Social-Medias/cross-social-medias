00254d63ec829fe733ff064f55b3176ef897a9a4
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-25 01:47:09 +0100

    create admin page. Minimalist

---
+++ pages/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Admin page</h1>
+    <p v-if="user">Hello, {{user.email}}</p>
+    <p v-else>The user is not authenticated!</p>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    user () { return (this.$store.state.auth || {}).user || null }
+  }
+}
+</script>
