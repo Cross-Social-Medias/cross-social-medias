e2b7395464924d70903ba863cda1c4b831322e91
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-27 01:37:24 +0100

    update i18n struct

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-     <a class="navbar-brand" href="#">POC cross social media</a>
+     <a class="navbar-brand" href="#">{{ $t('layouts.default.software_name') }}</a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
      </button>
@@ -9,20 +9,20 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">{{ $t('layouts.default.home') }}<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/twitter">Twitter</a></i>
+            <a class="nav-link" href="/twitter">{{ $t('layouts.default.twitter') }}</a></i>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#">Instagram</a>
+            <a class="nav-link disabled" href="#">{{ $t('layouts.default.instagram') }}</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#">Facebook</a>
+            <a class="nav-link disabled" href="#">{{ $t('layouts.default.facebook') }}</a>
           </li>
           <li class="divider-vertical"></li>
           <li class="nav-item">
-            <a class="nav-link" href="/mappings">Mappings</a>
+            <a class="nav-link" href="/mappings">{{ $t('layouts.default.mappings') }}</a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <p>{{ $t('pages.about.project_sentence') }}<a href="https://twitter.com/dupont_bertrand">@ber</a> {{ $t('general.and') }} <a href="https://twitter.com/zemog_emualliug">@zemog_emualliug</a></p>
     <p>
       {{ $t('general.go_back') }}
-      <nuxt-link to="/">{{ $t('links.home') }}</nuxt-link>
+      <nuxt-link to="/">{{ $t('layouts.default.home') }}</nuxt-link>
     </p>
   </div>
 </template>
