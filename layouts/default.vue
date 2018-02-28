067a0bf18a0a7cf0290620249f7e995e4023e7cc
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-01 00:27:18 +0100

    fix missing link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-     <a class="navbar-brand" href="#">{{ $t('layouts.default.software_name') }}</a>
+     <nuxt-link class="navbar-brand" to="/">{{ $t('layouts.default.software_name') }}</nuxt-link>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
      </button>
