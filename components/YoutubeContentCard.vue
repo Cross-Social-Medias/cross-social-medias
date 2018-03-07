73ca09c60770a8ce32400ce922952050dcc7cadf
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-08 00:22:14 +0100

    prepare new view

---
+++ components/YoutubeContentCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="card-body">
+      <a :href="`https://www.youtube.com/watch?v=${youtube.videoId}`" >
+        Link
+      </a>
+      <p>{{ youtube.title }}</p>
+      <p>{{ youtube.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      youtube: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
