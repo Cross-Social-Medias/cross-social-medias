6485dfb11d108928683af432a073b41a2182bbe2
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-08 08:54:49 +0100

    fill the youtube card

--- components/YoutubeContentCard.vue
+++ components/YoutubeContentCard.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <div class="card-body">
+      <img 
       <a :href="`https://www.youtube.com/watch?v=${youtube.videoId}`" >
-        Link
+        <img :src="youtube.thumbnail.url" alt="thumbnail" :height="youtube.thumbnail.height" :width="youtube.thumbnail.width">
       </a>
-      <p>{{ youtube.title }}</p>
-      <p>{{ youtube.description }}</p>
+      <p class="card-text">{{ youtube.title }}</p>
+      <text-formater :text="youtube.description" class="card-text"></text-formater>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">{{ youtube.publishedAt }}</small>
     </div>
   </div>
 </template>
 
 <script>
+  import TextFormater from "~/components/TextFormater.vue";
+
   export default {
+    components: {
+      TextFormater
+    },
     props: {
       youtube: {
         type: Object,
