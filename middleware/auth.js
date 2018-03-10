8db512a220a3a396d57d2022746b52a5a464ccab
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-10 16:53:40 +0100

    fix route redirection

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,19 @@
 export default function ({store, redirect, route}) {
-  const userIsLoggedIn = !!store.state.auth.user;
-  const urlRequiresAuth = /^\/admin(\/|$)/.test(route.fullPath);
-  const urlRequiresNonAuth = /^\/login(\/|$)/.test(route.fullPath);
+ const userIsLoggedIn = !!store.state.auth.user;
+
+  const authRoutes = ["admin", "mappings", "resume", "twitter"];
+  const authRegex = new RegExp(authRoutes.join("|")); // old request /^\/admin|mappings|resume|twitter(\/|$)/
+
+  const noAuthRoutes = ["login"];
+  const noAuthRegex = new RegExp(authRoutes.join("|")); // old request /^\/login(\/|$)/
+
+  const urlRequiresAuth = authRegex.test(route.fullPath);
+  const urlRequiresNonAuth = noAuthRegex.test(route.fullPath);
   if (!userIsLoggedIn && urlRequiresAuth) {
     return redirect('/login');
   }
   if (userIsLoggedIn && urlRequiresNonAuth) {
-    return redirect('/admin');
+    return route;
   }
   return Promise.resolve();
 }
