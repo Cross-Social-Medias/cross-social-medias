5acf93b6b791487f5d334a908a5558ce96b89b08
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-10 17:23:37 +0100

    add youtube tab + fix unused var

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 export default function ({store, redirect, route}) {
- const userIsLoggedIn = !!store.state.auth.user;
-
-  const authRoutes = ["admin", "mappings", "resume", "twitter"];
+  const userIsLoggedIn = !!store.state.auth.user;
+  
+  const authRoutes = ["admin", "mappings", "resume", "twitter", "youtube"];
   const authRegex = new RegExp(authRoutes.join("|")); // old request /^\/admin|mappings|resume|twitter(\/|$)/
 
   const noAuthRoutes = ["login"];
-  const noAuthRegex = new RegExp(authRoutes.join("|")); // old request /^\/login(\/|$)/
+  const noAuthRegex = new RegExp(noAuthRoutes.join("|")); // old request /^\/login(\/|$)/
 
   const urlRequiresAuth = authRegex.test(route.fullPath);
   const urlRequiresNonAuth = noAuthRegex.test(route.fullPath);
