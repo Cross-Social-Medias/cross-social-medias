53953aef36c1a4b7a52a50e17f8ab620df912ce3
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-13 00:58:41 +0100

    move a part of code

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
+import cookies from 'js-cookie';
+import {setAuthToken, resetAuthToken} from '~/utils/auth';
+
 export default {
   auth: {
     me: () => axios.get(`fakeUrl`),
     login: (data) => axios.post(`${process.env.serverUrl}/api/v1/sign_in`, data)
   }
+}
+
+const token = cookies.get('x-access-token');
+if (token) {
+  setAuthToken(token);
+} else {
+  resetAuthToken();
 }
