736687ad7159e9027cd02218983a623cea7628ff
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-13 00:01:20 +0100

    add tooltip + new field

--- pages/mapping_new.vue
+++ pages/mapping_new.vue
@@ -5,14 +5,22 @@
       <div class="form-group">
         <label for="mapping_name">{{ $t('pages.mapping_new.mapping_name') }}</label>
         <input v-model="mapping_name" class="form-control" id="mapping_name">
+        <small id="mapping_name_help" class="form-text text-muted">{{ $t('pages.mapping_new.mapping_name_help') }}</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">{{ $t('pages.mapping_new.twitter_username') }}</label>
+        <label for="twitter_username">{{ $t('pages.mapping_new.twitter_username') }}</label>
         <input v-model="twitter_username" class="form-control" id="twitter_username">
+        <small id="twitter_username_help" class="form-text text-muted">{{ $t('pages.mapping_new.twitter_username_help') }}</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">{{ $t('pages.mapping_new.instagram_username') }}</label>
+        <label for="instagram_username">{{ $t('pages.mapping_new.instagram_username') }}</label>
         <input v-model="instagram_username" class="form-control" id="instagram_username">
+        <small id="instagram_username_help" class="form-text text-muted">{{ $t('pages.mapping_new.instagram_username_help') }}</small>
+      </div>
+      <div class="form-group">
+        <label for="youtube_channel_id">{{ $t('pages.mapping_new.youtube_channel_id') }}</label>
+        <input v-model="youtube_channel_id" class="form-control" id="youtube_channel_id">
+        <small id="youtube_channel_id_help" class="form-text text-muted">{{ $t('pages.mapping_new.youtube_channel_id_help') }}</small>
       </div>
       <button type="submit" class="btn btn-primary">{{ $t('pages.mapping_new.submit') }}</button>
     </form>
@@ -26,7 +34,8 @@ export default {
       error: "",
       mapping_name: null,
       twitter_username: null,
-      instagram_username: null
+      instagram_username: null,
+      youtube_channel_id: null
     }
   },
   computed: {
@@ -38,6 +47,7 @@ export default {
         mapping_name: this.mapping_name,
         twitter_username: this.twitter_username,
         instagram_username: this.instagram_username,
+        youtube_channel_id: this.youtube_channel_id,
         created_by: this.user.id,
         user_id: this.user.id
       };
