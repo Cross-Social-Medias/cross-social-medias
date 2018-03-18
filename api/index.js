58aac7f7a859b273e469fde30044334a45d6a68c
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-18 18:35:37 +0100

    implement me call

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ import {setAuthToken, resetAuthToken} from '~/utils/auth';
 
 export default {
   auth: {
-    me: () => axios.get(`fakeUrl`),
+    me: () => axios.get(`${process.env.serverUrl}/api/v1/me`),
     login: (data) => axios.post(`${process.env.serverUrl}/api/v1/sign_in`, data)
   }
 }
 
 const token = cookies.get('x-access-token');
-const user = cookies.get('user');
+// const user = cookies.get('user');
 if (token) {
   setAuthToken(token);
 } else {
